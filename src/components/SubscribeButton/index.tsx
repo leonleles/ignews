54596800344dc30f2a1870b388aa4160c6c7ef6a
leonleles <leoleles9159@hotmail.com>
2021-06-18 22:45:45 -0300

    Checkout parcial

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,4 @@
+import { signIn, useSession } from 'next-auth/client';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -5,10 +6,22 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [session] = useSession();
+
+  function handleSubscribe () {
+      if(!session) {
+        signIn();
+        return;
+      }
+
+      
+  }
+
   return (
     <button
       className={styles.subscribeButton}
       type="button"
+      onClick={handleSubscribe}
     >
       Subscribe Now
     </button>
